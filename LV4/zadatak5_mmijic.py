4bb08f37736d51d9108dd0b885e9fa869bf11fba
marko <markomijic08@gmail.com>
2026-05-09 00:33:01 +0200

    Include R2 score in model evaluation
    Added R2 score evaluation for model performance.

--- LV4/zadatak5_mmijic.py
+++ LV4/zadatak5_mmijic.py
@@ -6,15 +6,14 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv('cars_processed.csv')
 
-# 1. Izbacivanje nepotrebnih stupaca (ostavljamo samo numeričke za ovaj zadatak)
-# Prema uputi izbacujemo 'name' i zadržavamo numeričke
+# 1.
 df_numeric = df.drop(['name', 'fuel', 'seller_type', 'transmission', 'owner'], axis=1)
 
 # Definicija ulaza i izlaza
 X = df_numeric.drop('selling_price', axis=1)
 y = df_numeric['selling_price']
 
-# 2. Podjela na train/test (80-20)
+# 2. Podjela na train/test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 3. Skaliranje
